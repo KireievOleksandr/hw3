0de9ba041c4b72393de12b305d631ad17a39c94e
Alexander Kireev <alexander.kireev@synebo.io>
2021-11-29 23:55:31 +0200

    Fallback function implemented

--- contracts/SpellTokenICO.sol
+++ contracts/SpellTokenICO.sol
@@ -8,43 +8,43 @@ interface GetStudents {
 }
 
 interface ERC20 {
-    function balanceOf(address _owner) external view returns (uint balance);
     function transfer(address _to, uint _value) external returns (bool success);
 }
 
 contract SpellTokenICO {
 
     AggregatorV3Interface internal priceFeed;
-    address studentContractAddress = 0x0E822C71e628b20a35F8bCAbe8c11F274246e64D;
-    address chainLinkETHUSDRinkeby = 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e;
-    address tokenAddress = 0x15B5B9CEDad6d2A5724392f4897EaB1e297a2838;
+    address studentContractAddress;
+    address tokenAddress;
 
-    constructor() {
-        priceFeed = AggregatorV3Interface(chainLinkETHUSDRinkeby);
-    }
-
-    function getLatestETHUSDPrice() public view returns (uint) {
-        ( uint80 roundID, int priceETHUSD, uint startedAt, uint timeStamp, uint80 answeredInRound ) = priceFeed.latestRoundData();
-        return uint(priceETHUSD);
-    }
-
-    function getStudentsCount() public view returns(uint) {
-        return GetStudents(studentContractAddress).getStudentsList().length;
-    }
-
-    function getTokenBalance() public view returns(uint) {
-        return ERC20(tokenAddress).balanceOf(address(this));
-    }
-
-    function calcTokenAmount(uint _eth) public view returns(uint) {
-        return _eth * getLatestETHUSDPrice() / getStudentsCount() / (10 ** priceFeed.decimals());
+    constructor(address _tokenAddress, address _studentContractAddress, address _chainLinkETHUSDRinkeby) {
+        priceFeed = AggregatorV3Interface(_chainLinkETHUSDRinkeby);
+        studentContractAddress = _studentContractAddress;
+        tokenAddress = _tokenAddress;
     }
 
     receive() external payable {
-        if(getTokenBalance() >= calcTokenAmount(msg.value)) {
-            ERC20(tokenAddress).transfer(msg.sender, calcTokenAmount(msg.value));
-        } else {
-            (bool sent, bytes memory data) = msg.sender.call{ value: msg.value }("Sorry, there is not enough tokens to buy");
+        send();
+    }
+
+    fallback() external payable {
+        send();
+    }
+
+    function send() private {
+        require(msg.value > 0, "Send ETH to buy some tokens");
+        
+        ( , int priceETHUSD, , , ) = priceFeed.latestRoundData();
+        uint studentsCount = GetStudents(studentContractAddress).getStudentsList().length;
+        uint tokenAmount =  msg.value * uint(priceETHUSD) / studentsCount / (10 ** priceFeed.decimals());
+
+        try ERC20(tokenAddress).transfer(msg.sender, tokenAmount) {
+        } catch Error(string memory) {
+            (bool success, ) = msg.sender.call{ value: msg.value }("Sorry, there is not enough tokens to buy");
+            require(success, "External call failed");
+        } catch (bytes memory reason) {
+            (bool success, ) = msg.sender.call{ value: msg.value }(reason);
+            require(success, "External call failed");
         }
     }
 }
